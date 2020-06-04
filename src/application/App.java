6a8226904312832eed966a39a7922148d925fd2d
gtiburcio <guilherme.tiburcio001@gmail.com>
2020-06-04 00:30:15 -0300

    Adicionando conexao com banco de dados

---
+++ src/application/App.java
@@ -0,0 +1,21 @@
+package application;
+
+import application.views.paciente.NovoPaciente;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene novoPacienteScene = new NovoPaciente().mountScene(stage);
+        stage.setScene(novoPacienteScene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
---
+++ src/application/dao/MySingletonConnection.java
@@ -0,0 +1,33 @@
+package application.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Objects;
+
+public class MySingletonConnection {
+
+    public Connection connection;
+
+    public static MySingletonConnection db;
+
+    private MySingletonConnection() {
+        String url = "jdbc:mysql://localhost:3306/";
+        String dbName = "consultorio";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "";
+        String password = "";
+        try {
+            Class.forName(driver).newInstance();
+            this.connection = DriverManager.getConnection(url.concat(dbName), userName, password);
+        } catch (Exception sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    public static synchronized MySingletonConnection getInstance() {
+        if (Objects.isNull(db)) {
+            db = new MySingletonConnection();
+        }
+        return db;
+    }
+}

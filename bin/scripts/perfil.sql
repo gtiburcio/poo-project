62f2a58bdabbf513407b5590bd475b3dc1d5f355
gtiburcio <guilherme.tiburcio001@gmail.com>
2020-06-08 17:55:00 -0300

    remove bin

--- bin/scripts/perfil.sql
+++
@@ -1,8 +0,0 @@
--- create perfil
-
-create table perfil(
-id_perfil bigint primary key auto_increment,
-descricao varchar(100) not null
-);
-
-insert into perfil(descricao) values('Secretária'), ('Médico');
